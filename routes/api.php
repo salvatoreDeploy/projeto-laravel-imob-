656b7f7a12340337eb545d557e0d13be33d8df12
salvatoreDeploy <liderhenrique@gmail.com>
2023-05-13 16:17:44 -0300

    feat: Companies api structure

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,9 @@ use Illuminate\Http\Request;
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
+
+Route::group(['namespace' => 'Api', 'as' => 'api.'], function (){
+    Route::apiResource('/companys', 'CompanyController');
+});
+
+
